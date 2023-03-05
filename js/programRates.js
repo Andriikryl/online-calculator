bc0e4083b7535cec246093395a58db1d88cb7b49
Amdrii <andreyk557@gmail.com>
2023-03-05 13:00:38 +0200

    feat[radio-btn-value]

---
+++ js/programRates.js
@@ -0,0 +1,25 @@
+const programBase = 0.12;
+const programIt = 0.047;
+const programGov = 0.067;
+const programZero = 0.108;
+
+
+document.querySelector('#base-value').value = programBase
+document.querySelector('#it-value').value = programIt
+document.querySelector('#gov-value').value = programGov
+document.querySelector('#zero-value').value = programZero
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
